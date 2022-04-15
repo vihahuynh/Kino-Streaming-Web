a4cab04c13c6757e5af4ba8bbd2c29774b74a60a
vihahuynh <huynhviha1703@gmail.com>
2022-04-15 16:19:41 +0700

    responsive newsletter

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,35 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import MoviesGrid from "../components/movies/moviesGrid/moviesGrid";
 import Wrapper from "../components/wrapper";
 import Loading from "../components/loading";
-import SearchBar from "../components/search/search";
 
 import { mediaTypeChange } from "../reducers/movie";
-import { filterRemove } from "../reducers/search";
 
 import classes from "./movies.module.css";
 
 const Movies = () => {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
-  const mediaType = useSelector((state) => state.movie.mediaType);
-  console.log(mediaType);
 
   useEffect(() => {
-    if (!mediaType) {
-      dispatch(filterRemove());
-    }
     dispatch(mediaTypeChange(true));
     setTimeout(() => setIsLoading(false), 300);
-  }, [dispatch, mediaType]);
+  }, [dispatch]);
 
   return (
     <Wrapper>
       <div className={classes.movies__title}>
         <h1>Movies</h1>
       </div>
-      <SearchBar />
       {isLoading ? <Loading height="70vh" /> : <MoviesGrid />}
     </Wrapper>
   );
--- src/pages/tvShows.js
+++ src/pages/tvShows.js
@@ -1,34 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import MoviesGrid from "../components/movies/moviesGrid/moviesGrid";
 import Wrapper from "../components/wrapper";
 import Loading from "../components/loading";
 
 import { mediaTypeChange } from "../reducers/movie";
-// import { filterRemove } from "../reducers/search";
 
 import classes from "./movies.module.css";
-import SearchBar from "../components/search/search";
 
 const TVShows = () => {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
-  const mediaType = useSelector((state) => state.movie.mediaType);
 
   useEffect(() => {
-    // if (mediaType) {
-    //   dispatch(filterRemove());
-    // }
     dispatch(mediaTypeChange(false));
     setTimeout(() => setIsLoading(false), 300);
-  }, [dispatch, mediaType]);
+  }, [dispatch]);
 
   return (
     <Wrapper>
       <div className={classes.movies__title}>
         <h1>tv shows</h1>
       </div>
-      <SearchBar />
       {isLoading ? <Loading height="70vh" /> : <MoviesGrid />}
     </Wrapper>
   );
