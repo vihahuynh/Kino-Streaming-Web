4bc5c8f86db839adbcd21c0367bade367c326d08
vihahuynh <huynhviha1703@gmail.com>
2022-04-17 17:10:59 +0700

    update images

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,9 @@ import React from "react";
 import classes from "./logo.module.css";
 
 const Logo = () => {
-  return <img className={classes.logo} alt="Logo" src="images/logo.png" />;
+  return (
+    <img className={classes.logo} alt="Logo" src="%PUBLIC_URL%/logo.png" />
+  );
 };
 
 export default Logo;
