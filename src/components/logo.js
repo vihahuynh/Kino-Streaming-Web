2c95f34f1697bda12a295c90d13a07fd3e0b64f1
vihahuynh <huynhviha1703@gmail.com>
2022-04-17 17:19:59 +0700

    update all images

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,11 @@ import classes from "./logo.module.css";
 
 const Logo = () => {
   return (
-    <img className={classes.logo} alt="Logo" src="%PUBLIC_URL%/logo.png" />
+    <img
+      className={classes.logo}
+      alt="Logo"
+      src={`${process.env.PUBLIC_URL}/logo.png`}
+    />
   );
 };
 
